2379fb65747fc21d9869406dab2de04bb21ae7e8
zWesleyDavid <wesleydavid1a@gmail.com>
2024-11-05 23:46:03 -0300

    feito alguns testes

--- monesy/test/despesas_screen_test.dart
+++ monesy/test/despesas_screen_test.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:monesy/domain/despesas.dart';
+import 'package:monesy/screens/despesas_screen.dart';
 import 'package:monesy/service/api_service.dart';
 
 class MyMockClient extends Mock implements Client {}
@@ -13,8 +16,8 @@ void main() {
     Uri uri = Uri.parse("http://localhost:3000/despesas");
 
     when(() => mockClient.get(uri)).thenAnswer((_) async => Response(
-        '{"id": "3a26", "titulo": "Luz", "descricao": "Conta de luz", "valor": 150}', 200
-    ));
+        '{"id": "3a26", "titulo": "Luz", "descricao": "Conta de luz", "valor": 150}',
+        200));
 
     ApiService service = ApiService(mockClient);
 
@@ -26,9 +29,50 @@ void main() {
     expect(resultado.valor, 150);
   });
 
-  test('', () async {});
+  test('getDespesa lança exceção ao receber erro de servidor', () async {
+    MyMockClient client = MyMockClient();
+    final apiService = ApiService(client);
+    when(() => client.get(Uri.parse('http://localhost:3000/despesas')))
+        .thenAnswer((_) async => Response('Erro', 500));
 
-  test('', () async {});
+    expect(() => apiService.getDespesa(), throwsException);
+  });
 
-  test('', () async {});
+  test('fetchDespesas aplica filtro de valor mínimo', () async {
+    MyMockClient client = MyMockClient();
+    final apiService = ApiService(client);
+    final despesasData = [
+      {'id': '1', 'titulo': 'Aluguel', 'valor': 1200.0},
+      {'id': '2', 'titulo': 'Mercado', 'valor': 200.0}
+    ];
+    when(() => client.get(Uri.parse('http://localhost:3000/despesas')))
+        .thenAnswer((_) async => Response(json.encode(despesasData), 200));
+
+    final despesas = await apiService.fetchDespesas();
+    final despesasFiltradas = despesas.where((d) => d['valor'] > 500).toList();
+
+    expect(despesasFiltradas.length, 1);
+    expect(despesasFiltradas[0]['titulo'], 'Aluguel');
+  });
+
+  testWidgets('DespesaListScreen carrega a lista vazia inicialmente',
+      (WidgetTester tester) async {
+    MyMockClient client = MyMockClient();
+    when(() => client.get(Uri.parse('http://localhost:3000/despesas')))
+        .thenAnswer((_) async => Response('[]', 200));
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: DespesaListScreen(),
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.text('Despesas'), findsOneWidget);
+    expect(
+        find.byType(ListTile), findsNothing); // A lista está inicialmente vazia
+    expect(find.byType(FloatingActionButton), findsOneWidget);
+  });
+
 }
